a32eeded4f7dfd2b0c04bf537505dd7b2de6cf21
Jason <1075850619@qq.com>
2020-11-30 14:16:55 +0800

    增加了所属行业和行业市盈率数据的爬取

--- code/com/blue/stock/get_stock.py
+++ code/com/blue/stock/get_stock.py
@@ -52,11 +52,11 @@ def getDBConnect(db_conf):
 
 def formatData(infoDict):
     for item in infoDict:
-        if item in ['股票名称', '股票代码', '日期']:
+        if item in ['股票名称', '股票代码', '日期', '所属行业']:
             continue
         value = infoDict[item]
-        value = re.sub(r'[^-?\d+.?\d*]|[--]', '', value)
-        if value == '':
+        value = re.sub(r'[^-?\d+.?\d*]|[-]{2}', '', value)
+        if value == '' or value is None:
             value = 'null'
         infoDict[item] = value
 
@@ -77,17 +77,20 @@ def createTable(db):
             日期   VARCHAR(10) COMMENT '日期',
             股票代码 VARCHAR(10) COMMENT '股票代码',
             股票名称 VARCHAR(50) COMMENT '股票名称',
+            所属行业 VARCHAR(100) COMMENT '所属行业',
+            行业平均市盈率 DECIMAL(19, 2) COMMENT '行业平均市盈率',
+            行业扣除后平均市盈率 DECIMAL(19, 2) COMMENT '行业扣除后平均市盈率',
             股价   DECIMAL(19, 2) COMMENT '股价',
             股价波动 DECIMAL(19, 2) COMMENT '股价波动',
             股价波幅 DECIMAL(19, 2) COMMENT '股价波幅',
             今开   DECIMAL(19, 2) COMMENT '今开',
             昨收   DECIMAL(19, 2) COMMENT '昨收',
-            最高   DECIMAL(19, 2) COMMENT '最高',
+            最高   DECIMAL(19, 4) COMMENT '最高',
             最低   DECIMAL(19, 2) COMMENT '最低',
             振幅   DECIMAL(19, 2) COMMENT '振幅',
             换手率  DECIMAL(19, 2) COMMENT '换手率',
             成交量  DECIMAL(19, 2) COMMENT '成交量',
-            成交额  DECIMAL(19, 2) COMMENT '成交额',
+            成交额  DECIMAL(19, 4) COMMENT '成交额',
             市盈率  DECIMAL(19, 2) COMMENT '市盈率',
             市净率  DECIMAL(19, 2) COMMENT '市净率'
         ) COMMENT '股票行情表'
@@ -113,7 +116,9 @@ def getStockInfo(stock_list):
             # 每个股票存为字典，数据处理较麻烦，有些数据有“杂音”，需单独给出if判断，或在正则中约束
             infoDict = {}
             soup = BeautifulSoup(html, "html.parser")
-            stockInfo = soup.find('div', attrs={'id': 'act_quote'})
+            stockInfo = soup.find('div', attrs={'id': 'act_quote'}).find('table').find('table')
+            # print(stockInfo)
+
             name = stockInfo.find('div', attrs={'class': 'Lfont'}).string
             infoDict.update({'股票代码': re.sub(r"\D+", "", name)})
             infoDict.update({'股票名称': re.sub(r"\d+|\(|\)", "", name)})
@@ -124,12 +129,14 @@ def getStockInfo(stock_list):
             price = re.sub(r'[^-?\d+.?\d*]', '', price.get_text())
             hq_date = hq_time.get_text().split(' ')[0]
 
+            if hq_date is None or hq_date == '' or hq_date == '--':
+                continue
             infoDict.update({'股价': price})
             infoDict.update({'日期': hq_date})
             infoDict.update({'股价波动': chg.contents[0]})
             infoDict.update({'股价波幅': chg.contents[2]})
 
-            stockDetialInfo = stockInfo.find('table', attrs={'id': 'quotetab_stock'})
+            stockDetialInfo = stockInfo.find('td', attrs={'class': 'Rlist'})
             td = stockDetialInfo.find_all("td")
             for item in td:
                 text = item.get_text()
@@ -144,6 +151,11 @@ def getStockInfo(stock_list):
 
                 infoDict[key] = real_val
 
+            industryInfo = stockInfo.find(lambda e: e.name == 'td' and '所属行业' in e.text).find(lambda e: e != 'span')
+            infoDict['所属行业'] = industryInfo.next
+            infoDict['行业平均市盈率'] = re.split(':|：', industryInfo.next.next.next)[1]
+            infoDict['行业扣除后平均市盈率'] = re.split(':|：', industryInfo.next.next.next.next.next)[1]
+
             # print(infoDict)
             # {'股票代码': '600004', '股票名称': '白云机场', '股价': '15.63', '日期': '2020-06-16', '股价波动': '0.42', '股价波幅': '2.76%',
             #  '今开': '15.40', '最高': '16.17', '振幅': '5.19%', '换手率': '0.63%', '昨收': '15.21', '最低': '15.38',
@@ -151,24 +163,30 @@ def getStockInfo(stock_list):
             #  '2020-03-31 每股收益': '-0.03元'}
 
             infoDict = formatData(infoDict)
+            # print(infoDict)
 
-            valueString = "INSERT INTO stock.stock(日期,股票代码,股票名称,股价,股价波动,股价波幅,今开,昨收,最高,最低,振幅,换手率,成交量,成交额,市盈率,市净率) " \
-                          "values ('{}','{}','{}',{},{},{},{},{},{},{},{},{},{},{},{},{});" \
-                .format(infoDict['日期'], infoDict['股票代码'], infoDict['股票名称'], infoDict['股价'], infoDict['股价波动'],
-                        infoDict['股价波幅'], infoDict['今开'], infoDict['昨收'], infoDict['最高'], infoDict['最低'],
-                        infoDict['振幅'], infoDict['换手率'], infoDict['成交量'], infoDict['成交额'], infoDict['市盈率'],
-                        infoDict['市净率'])
+            valueString = "INSERT INTO stock.stock(日期,股票代码,股票名称,所属行业,行业平均市盈率,行业扣除后平均市盈率,股价,股价波动,股价波幅,今开,昨收,最高,最低,振幅,换手率,成交量,成交额,市盈率,市净率) " \
+                          "values ('{}','{}','{}','{}',{},{},{},{},{},{},{},{},{},{},{},{},{},{},{});" \
+                .format(infoDict.get('日期', 'null'), infoDict.get('股票代码', 'null'), infoDict.get('股票名称', 'null'),
+                        infoDict.get('所属行业', 'null'), infoDict.get('行业平均市盈率', 'null'),
+                        infoDict.get('行业扣除后平均市盈率', 'null'),
+                        infoDict.get('股价', 'null'), infoDict.get('股价波动', 'null'), infoDict.get('股价波幅', 'null'),
+                        infoDict.get('今开', 'null'), infoDict.get('昨收', 'null'),
+                        infoDict.get('最高', 'null'), infoDict.get('最低', 'null'),
+                        infoDict.get('振幅', 'null'), infoDict.get('换手率', 'null'), infoDict.get('成交量', 'null'),
+                        infoDict.get('成交额', 'null'), infoDict.get('市盈率', 'null'),
+                        infoDict.get('市净率', 'null'))
 
-            # print(valueString)
             db.cursor().execute(valueString)
 
             ready_count += 1
-            print('\r当前第{0:}个,共{1:}个.stock_code:{2:}'.format(ready_count, all_count,stock_code))  # 打印进度
+            print('\r当前第{0:}个,共{1:}个.stock_code:{2:}'.format(ready_count, all_count, stock_code))  # 打印进度
             if ready_count % 100 == 0:
                 db.commit()
-        except:
+        except AttributeError as e:
             ready_count += 1
-            print('\r当前第{0:}个,共{1:}个.stock_code:{2:}'.format(ready_count, all_count,stock_code))
+            print(
+                '\r当前第{0:}个,共{1:}个.stock_code:{2:} .exception msg: 解析html异常'.format(ready_count, all_count, stock_code))
             traceback.print_exc()
     db.commit()
 
